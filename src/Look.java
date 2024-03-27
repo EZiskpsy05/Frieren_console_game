853a850c8d2e5d5a5ef20fd82b58b140ed8b083c
Howard Tran <anhkhoat03207@gmail.com>
2024-03-27 18:37:11 -0400

    Update Look.java

--- src/Look.java
+++ src/Look.java
@@ -1,43 +1,46 @@
-
 public class Look implements Command {
 
-	String[] synonyms = new String[]{"look around","look","observe","see","watch"};
-	String[] args;
-	Location curr;
+    // Array of synonyms for the look command
+    String[] synonyms = new String[]{"look around","look","observe","see","watch"};
+    String[] args;
+    Location curr;
 
-	public Look(Location l) {
-		this.curr = l;
-	}
+    // Constructor to initialize the current location
+    public Look(Location l) {
+        this.curr = l;
+    }
 
-	public Boolean isSynonym(String cmd)
-	{
-		for(String s:this.synonyms)
-			if(cmd.equalsIgnoreCase(s))
-				return true;
-		return false;
-	}
+    // Method to check if the given command is a synonym for look
+    public Boolean isSynonym(String cmd) {
+        for (String s : this.synonyms)
+            if (cmd.equalsIgnoreCase(s))
+                return true;
+        return false;
+    }
 
-	public Location execute(String arg)
-	{
-		this.args = arg.split(" ");
-		if(args.length == 1) // look at location
-		{
-			System.out.println("\n\t\t"+ curr.name);
-			curr.look();
-		}
-		else	// look at item
-		{
-			Boolean validItem = false;
-			
-			for(int i = 0; i < curr.items.size(); i++)
-				if(arg.toLowerCase().contains(curr.items.get(i).name.toLowerCase())) {
-					System.out.println(curr.items.get(i).desc);
-					validItem = true;
-				}
+    // Method to execute the look command
+    public Location execute(String arg) {
+        this.args = arg.split(" ");
+        if(args.length == 1) // Look at location
+        {
+            System.out.println("\n\t\t"+ curr.name);
+            curr.look(); // Display information about the current location
+        }
+        else // Look at item
+        {
+            Boolean validItem = false;
 
-			if(!validItem)
-				System.out.println("You see no such thing!");
-		}
-		return curr;
-	}
+            for(int i = 0; i < curr.items.size(); i++) {
+                // Check if the argument contains the name of an item in the current location
+                if(arg.toLowerCase().contains(curr.items.get(i).name.toLowerCase())) {
+                    System.out.println(curr.items.get(i).desc); // Display the description of the item
+                    validItem = true;
+                }
+            }
+
+            if(!validItem)
+                System.out.println("You see no such thing!"); // If the item is not found, display a message
+        }
+        return curr; // Return the current location
+    }
 }
