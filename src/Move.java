9d78903f2a240bbb7f82a049f9b9bf0796838b3a
Howard Tran <anhkhoat03207@gmail.com>
2024-03-27 18:38:08 -0400

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -1,40 +1,43 @@
-
 public class Move implements Command {
 
-	String[] synonyms = new String[]{"move","go","walk","proceed","advance"};
-	String[] args;
-	Location curr;
+    // Array of synonyms for the move command
+    String[] synonyms = new String[]{"move","go","walk","proceed","advance"};
+    String[] args;
+    Location curr;
 
-	public Move(Location l) {
-		this.curr = l;
-	}
+    // Constructor to initialize the current location
+    public Move(Location l) {
+        this.curr = l;
+    }
 
-	public Boolean isSynonym(String cmd)
-	{
-		for(String s:this.synonyms)
-			if(cmd.equalsIgnoreCase(s))
-				return true;
-		return false;
-	}
+    // Method to check if the given command is a synonym for move
+    public Boolean isSynonym(String cmd) {
+        for (String s : this.synonyms)
+            if (cmd.equalsIgnoreCase(s))
+                return true;
+        return false;
+    }
 
-	public Location execute(String arg)
-	{
-		this.args = arg.split(" ");
+    // Method to execute the move command
+    public Location execute(String arg) {
+        this.args = arg.split(" ");
 
-		if (arg.toLowerCase().contains("north") && curr.north != null){ 
-			curr = curr.north;	curr.enter();
-		}
-		else if(arg.toLowerCase().contains("south") && curr.south != null){
-			curr = curr.south;	curr.enter(); 
-		}
-		else if(arg.toLowerCase().contains("east") && curr.east != null) {
-			curr = curr.east;	curr.enter(); 
-		}
-		else if(arg.toLowerCase().contains("west") && curr.west != null){ 
-			curr = curr.west;	curr.enter(); 
-		}
-		else
-			System.out.println("You cannot go there!"); 
-		return curr;
-	}
+        // Check if the argument contains a direction and if the direction is valid, move to the new location
+        if (arg.toLowerCase().contains("north") && curr.north != null) {
+            curr = curr.north;
+            curr.enter(); // Enter the new location and display its description
+        } else if(arg.toLowerCase().contains("south") && curr.south != null) {
+            curr = curr.south;
+            curr.enter();
+        } else if(arg.toLowerCase().contains("east") && curr.east != null) {
+            curr = curr.east;
+            curr.enter();
+        } else if(arg.toLowerCase().contains("west") && curr.west != null) {
+            curr = curr.west;
+            curr.enter();
+        } else {
+            System.out.println("You cannot go there!"); // If the direction is not valid, display a message
+        }
+        return curr; // Return the current location
+    }
 }
