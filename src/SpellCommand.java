cfc0c3f76ff25d9a6687d39ab77adccd0742b94f
Howard Tran <anhkhoat03207@gmail.com>
2024-03-28 06:57:40 -0400

    Update SpellCommand.java

--- src/SpellCommand.java
+++ src/SpellCommand.java
@@ -1,63 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class SpellCommand implements Command{
+public class SpellCommand implements Command {
 
-	String[] synonyms = new String[]{"zoltraak","doragate","catastravia","mistilziela","shield","sorganeil","erfassen","sus","photo"};
-	String[] args;
-	Location curr;
+    private static final Map<String, Spell> SPELLS = new HashMap<>();
+    static {
+        SPELLS.put("zoltraak", new Zoltraak());
+        SPELLS.put("doragate", new Doragate());
+        SPELLS.put("catastravia", new Catastravia());
+        SPELLS.put("mistilziela", new Mistilziela());
+        SPELLS.put("shield", new ExpectoPatronum());
+        SPELLS.put("sorganeil", new ExpectoPatronum());
+        SPELLS.put("erfassen", new ExpectoPatronum());
+        SPELLS.put("sus", new ExpectoPatronum());
+        SPELLS.put("photo", new ExpectoPatronum());
+    }
 
-	public SpellCommand(Location l) {
-		this.curr = l;
-	}
-	
-	Zoltraak atk1 = new Zoltraak();
-	Doragate atk2 = new Doragate();
-	Catastravia atk3 = new Catastravia();
-	Mistilziela def1 = new Mistilziela();
-	ExpectoPatronum expat = new ExpectoPatronum();
+    private Location curr;
 
-	public Location execute(String args) {
+    public SpellCommand(Location curr) {
+        this.curr = curr;
+    }
 
-		if(args.split(" ")[0].equalsIgnoreCase(synonyms[0])) 
-			this.atk1.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[1])) 
-			this.atk2.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[2])) 
-			this.atk3.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[3]))
-			this.def1.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[4]))
-			this.expat.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[5]))
-			this.expat.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[6]))
-			this.expat.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[7]))
-			this.expat.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[8]))
-			this.expat.cast();
-		else if(args.split(" ")[0].equalsIgnoreCase(synonyms[3]))
-			this.expat.cast();
-		
-		
-		if(curr.p.size() > 0) {
-			if(curr.p.size() > 1)
-				System.out.println("You defeated " + curr.p.get(1).name);
-			else
-				System.out.println("You defeated " + curr.p.get(0).name);
-			
-			curr.isEnemyDefeated = true;
-		}
+    @Override
+    public Location execute(String args) {
+        String spellName = args.trim().toLowerCase().split(" ")[0];
+        Spell spell = SPELLS.get(spellName);
+        if (spell != null) {
+            spell.cast();
+        }
 
-		return curr;
-	}
+        if (curr.p.size() > 0) {
+            String enemyName = curr.p.size() > 1 ? curr.p.get(1).name : curr.p.get(0).name;
+            System.out.println("You defeated " + enemyName);
+            curr.isEnemyDefeated = true;
+        }
 
+        return curr;
+    }
 
-	public Boolean isSynonym(String cmd)
-	{
-		for(String s:this.synonyms)
-			if(cmd.equalsIgnoreCase(s))
-				return true;
-		return false;
-	}
-
+    @Override
+    public Boolean isSynonym(String cmd) {
+        return SPELLS.containsKey(cmd.toLowerCase());
+    }
 }
