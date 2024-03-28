34f6369342f8c6a5f0b555619f9232927d8a928e
Howard Tran <anhkhoat03207@gmail.com>
2024-03-28 06:53:17 -0400

    Update ControlPanel.java

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,25 +1,34 @@
+/**
+ * ControlPanel class manages the execution of commands in the game.
+ */
 public class ControlPanel {
-	Command [] cmds;
-	Location curr;
-	
-	// Constructor for ControlPanel class
-	public ControlPanel(Command [] cmds, Location l) {
-		this.cmds = cmds;
-		this.curr = l;
-	}
-	
-	// Method to parse and execute commands
-	public Location parse(String cmd){
+    Command[] commands; // Array of commands available
+    Location currentLocation; // Current location in the game
 
-		String op = cmd.split(" ")[0];
-		
-		for(int i = 0; i < cmds.length; i++)
-		{
-			if(cmds[i].isSynonym(op)) {
-				return cmds[i].execute(cmd);
-			}
-		}
-		System.out.println("I do not understand that. You could try the help command");
-		return curr;
-	}
+    /**
+     * Constructor for ControlPanel class.
+     * @param commands Array of commands available.
+     * @param location Current location in the game.
+     */
+    public ControlPanel(Command[] commands, Location location) {
+        this.commands = commands;
+        this.currentLocation = location;
+    }
+
+    /**
+     * Parses and executes a command.
+     * @param command The command to parse and execute.
+     * @return The new location after executing the command.
+     */
+    public Location parse(String command) {
+        String operation = command.split(" ")[0];
+
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i].isSynonym(operation)) {
+                return commands[i].execute(command);
+            }
+        }
+        System.out.println("I do not understand that. You could try the help command");
+        return currentLocation;
+    }
 }
